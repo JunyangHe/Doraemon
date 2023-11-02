5c1898854aef0517069480b19558b3cbe365fe86
Junyang He <106281470+JunyangHe@users.noreply.github.com>
2023-11-02 19:38:12 +0000

    Set up React

--- Doraemon/Doraemon/settings.py
+++ Doraemon/Doraemon/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "info.apps.InfoConfig",
-    "rest_framework"
+    "rest_framework",
+    "frontend.apps.FrontendConfig"
 ]
 
 MIDDLEWARE = [
